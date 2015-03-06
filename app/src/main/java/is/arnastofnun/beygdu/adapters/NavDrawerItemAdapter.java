bc9653ae91d09bd9eb6231b2e7c07d44a2078870
Daniel Pall Johannsson <danpjoh@gmail.com>
2015-03-06 11:50:50 +0000

    Adapter for the NavDrawer items should be ready.

--- app/src/main/java/is/arnastofnun/beygdu/adapters/NavDrawerItemAdapter.java
+++ app/src/main/java/is/arnastofnun/beygdu/adapters/NavDrawerItemAdapter.java
@@ -1,7 +1,68 @@
 package is.arnastofnun.beygdu.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import is.arnastofnun.beygdu.R;
+
 /**
- * Created by Notandi on 6.3.2015.
+ * @author Daniel Pall Johannsson
+ * @since 06.03.2015
+ * @version 1.0
+ *
+ * NavDrawerItemAdapter maps the item to the list view in each layout.
  */
-public class NavDrawerItemAdapter {
+public class NavDrawerItemAdapter extends BaseAdapter{
+
+    // An ArrayList that contains the information to put into each item
+    private ArrayList<String> itemList;
+    Context context;
+
+    public NavDrawerItemAdapter(ArrayList<String> itemList, Context context) {
+        this.itemList = itemList;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return itemList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return itemList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+
+        // Reuse the view if it is empty
+        if(v == null){
+            LayoutInflater vi = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = vi.inflate(R.layout.nav_drawer_item,null);
+        }
+
+        // Get the Text view we want to populate with our data
+        TextView textView = (TextView) v.findViewById(R.id.navDrawerItem);
+
+        // Get the String at the designated position
+        String itemTitle = itemList.get(position);
+
+        // Set the Text view with the itemTitle we got
+        textView.setText(itemTitle);
+        
+        return v;
+    }
 }
