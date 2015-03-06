e6e794b5fc4030e7bdcab4a24fe487cc011510f5
Daniel Pall Johannsson <danpjoh@gmail.com>
2015-03-06 10:38:41 +0000

    Start of Navigation Drawer class

---
+++ app/src/main/java/is/arnastofnun/beygdu/is/arnastofnun/beygdur/soonToBeAdded/NavDrawer.java
@@ -0,0 +1,12 @@
+package is.arnastofnun.beygdu.is.arnastofnun.beygdur.soonToBeAdded;
+
+/**
+ * @author Daniel Pall Johannsson
+ * @since 06.03.2015
+ * @version 1.0
+ *
+ * NavDrawer handles the navigation drawer and its functions
+ */
+public class NavDrawer {
+
+}
