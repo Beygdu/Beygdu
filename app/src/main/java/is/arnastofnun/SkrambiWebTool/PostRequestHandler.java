42a31b07368bfc77f7ba19fc34ef88b520e0dbb9
ArnJonsson <arj26@hi.is>
2015-03-10 19:30:06 +0000

    Some Work. Skrambi Not Online

--- app/src/main/java/is/arnastofnun/SkrambiWebTool/PostRequestHandler.java
+++ app/src/main/java/is/arnastofnun/SkrambiWebTool/PostRequestHandler.java
@@ -1,7 +1,97 @@
 package is.arnastofnun.SkrambiWebTool;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by arnarjons on 10.3.2015.
  */
 public class PostRequestHandler {
+
+    private URL url;
+    private HttpURLConnection conn = null;
+    private String targetUrl;
+    private String urlParam;
+    private String contentType;
+    private String contentLanguage;
+    private Boolean useCaches;
+    private Boolean doInput;
+    private Boolean doOutput;
+
+    private String contentLength;
+
+    public PostRequestHandler(String targetUrl, String urlParam, String contentType, String contentLanguage, Boolean useCaches, Boolean doInput, Boolean doOutput) {
+
+        this.targetUrl = targetUrl;
+        this.urlParam = urlParam;
+        this.contentType = contentType;
+        this.contentLanguage = contentLanguage;
+        this.useCaches = useCaches;
+        this.doInput = doInput;
+        this.doOutput = doOutput;
+
+        int contLength = urlParam.getBytes().length;
+        this.contentLength = Integer.toString(contLength);
+
+    }
+
+    public String sendRequest() {
+
+        String responseLine;
+        StringBuffer result;
+        try {
+            this.url = new URL(this.targetUrl);
+            this.conn = (HttpURLConnection) url.openConnection();
+            this.conn.setRequestMethod("POST");
+
+            this.conn.setRequestProperty("Content-Type", this.contentType);
+            this.conn.setRequestProperty("Content-Type", this.contentLength);
+            this.conn.setRequestProperty("Content-Language", this.contentLanguage);
+            this.conn.setUseCaches(this.useCaches);
+            this.conn.setDoInput(this.doInput);
+            this.conn.setDoOutput(this.doOutput);
+
+
+
+
+
+            DataOutputStream outStream = new DataOutputStream(this.conn.getOutputStream());
+            outStream.writeBytes(this.urlParam);
+            outStream.flush();
+            outStream.close();
+
+
+
+
+
+            InputStream inStream = this.conn.getInputStream();
+            BufferedReader bR = new BufferedReader(new InputStreamReader(inStream));
+
+            result = new StringBuffer();
+            while( (responseLine = bR.readLine()) != null ) {
+
+                result.append(responseLine);
+                result.append('\r');
+
+            }
+            bR.close();
+
+            return result.toString();
+
+        }
+        catch( Exception e ) {
+            return "failed to reach host";
+        }
+        finally {
+            if(this.conn != null) {
+                this.conn.disconnect();
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/is/arnastofnun/SkrambiWebTool/SkrambiWT.java
+++ app/src/main/java/is/arnastofnun/SkrambiWebTool/SkrambiWT.java
@@ -1,7 +1,50 @@
 package is.arnastofnun.SkrambiWebTool;
 
+import android.app.Activity;
+import android.os.AsyncTask;
+
+import is.arnastofnun.beygdu.MainActivity;
+
 /**
  * Created by arnarjons on 10.3.2015.
  */
-public class SkrambiWT {
+public class SkrambiWT extends AsyncTask<Void, Void, Void> {
+
+    public Activity activity;
+
+    private String word;
+    private String URL;
+
+    private String responseString;
+
+
+    public SkrambiWT(Activity activity, String word) {
+
+        this.activity = activity;
+
+        this.URL = "http://skrambi.arnastofnun.is/checkDocument";
+        this.word = word;
+
+    }
+
+    @Override
+    protected  void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Void doInBackground(Void... args) {
+        PostRequestHandler pHandler = new PostRequestHandler(this.URL, this.word,
+                "text/plain", "en-US", false, true, true);
+        this.responseString = pHandler.sendRequest();
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void args) {
+        String returnString = this.responseString;
+        //activity.(returnString);
+    }
+
+
 }
