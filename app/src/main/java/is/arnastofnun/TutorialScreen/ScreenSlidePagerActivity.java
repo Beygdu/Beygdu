769f13eef8c65ac1fd68186f60aeedd790c0e4d7
ArnJonsson <arj26@hi.is>
2015-04-01 17:58:13 +0000

    Basic Layout for a pageViewThingy

---
+++ app/src/main/java/is/arnastofnun/TutorialScreen/ScreenSlidePagerActivity.java
@@ -0,0 +1,61 @@
+package is.arnastofnun.TutorialScreen;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import is.arnastofnun.beygdu.R;
+
+public class ScreenSlidePagerActivity extends FragmentActivity {
+
+    private static final int NUM_PAGES = 5;
+
+    private ViewPager mPager;
+
+    private PagerAdapter mPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_slide);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mPager.getCurrentItem() == 0) {
+            super.onBackPressed();
+        }
+        else {
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new ScreenSlidePageFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+
+
+}
