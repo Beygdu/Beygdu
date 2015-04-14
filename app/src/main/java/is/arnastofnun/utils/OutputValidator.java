16345acbeb5ecea7220cd7a8498c8f5471015a85
ArnJonsson <arj26@hi.is>
2015-04-14 06:57:00 +0000

    Class OutputValidator created, intedned for Skrambi response

---
+++ app/src/main/java/is/arnastofnun/utils/OutputValidator.java
@@ -0,0 +1,38 @@
+package is.arnastofnun.utils;
+
+/**
+ * Created by arnarjons on 14.4.2015.
+ */
+public class OutputValidator {
+
+    ///
+    // Error Handling
+    ///
+    private boolean isError = false;
+    private String errorString = null;
+    public static final String OUTPUTVALIDATOR_ERROR_1 = "";
+    public static final String OUTPUTVALIDATOR_ERROR_2 = "";
+
+    ///
+    // Validators variables
+    ///
+    private String inputString;
+
+    public OutputValidator(String inputString) {
+        this.inputString = inputString;
+
+
+    }
+
+    ///
+    // Get/Set Methods
+    ///
+
+    ///
+    // Validators inner logic
+    ///
+
+    ///
+    // Result Handling
+    ///
+}
