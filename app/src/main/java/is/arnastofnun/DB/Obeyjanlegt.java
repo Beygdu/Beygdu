6622dd559d5db4b407f6707413bf0ffb92427ce7
ArnJonsson <arj26@hi.is>
2015-08-23 17:49:16 +0000

    Obeygjanlegt.cass added to get rid of try - catch statement in MainActivity.class

--- app/src/main/java/is/arnastofnun/DB/Obeyjanlegt.java
+++ app/src/main/java/is/arnastofnun/DB/Obeyjanlegt.java
@@ -1,7 +1,30 @@
 package is.arnastofnun.DB;
 
+import android.content.Context;
+import android.util.Log;
+
 /**
  * Created by arnarjons on 23.8.2015.
+ * Tilgangurinn med thessu er ad komast framhja try - catch i mainactivity
  */
 public class Obeyjanlegt {
+
+    private Context context;
+
+    public Obeyjanlegt(Context context) {
+        this.context = context;
+    }
+
+    public boolean isObeygjanlegt(String word) {
+
+        try {
+            DBController controller = new DBController(context);
+            if( controller.fetchObeygjanlegt(word) == null ) return false;
+            else return true;
+        }
+        catch (Exception e) {
+            Log.w("Obeygjanleg ordaleit", e);
+        }
+        return false;
+    }
 }
